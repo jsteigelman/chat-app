f6dbf0350a6d4e36c413f9aeb3cb60409d26beac
Joey Steigelman <joey.steigelman@gmail.com>
2022-01-19 16:26:53 -0500

    handle form and button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,19 +1,34 @@
 const socket = io()
 
+// elements with dollar sign naming convention
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $shareLocationButton = document.querySelector('#share-location')
+
 // send data (print a message) from server to client
 socket.on('message', (message) => {
     console.log(message)
 })
 
 // send data from client to server
-document.querySelector('#message-form').addEventListener('submit', (event) => {
+$messageForm.addEventListener('submit', (event) => {
     
     // prevent page from refreshing on form submission
     event.preventDefault() 
 
+    // disable submit button
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
     // send data (chat message) from client to server
     const chatMessage = event.target.elements.message.value
     socket.emit('sendChatMessage', chatMessage, (error) => {
+
+        // re-enable submit button and clear input
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
         if (error) {
             return console.log(error)
         }
@@ -23,12 +38,15 @@ document.querySelector('#message-form').addEventListener('submit', (event) => {
 })
 
 // share user location
-document.querySelector('#share-location').addEventListener('click', () => {
+$shareLocationButton.addEventListener('click', () => {
     
     // no support for sharing location
     if (!navigator.geolocation) {
         return alert('Sorry, geolocation is not supported by your browser.')
     }
+
+    // disable location button
+    $shareLocationButton.setAttribute('disabled', 'disabled')
 
     // send data (location) from client to server
     navigator.geolocation.getCurrentPosition((position) => {
@@ -37,6 +55,8 @@ document.querySelector('#share-location').addEventListener('click', () => {
             longitude: position.coords.longitude
         }, () => {
             console.log('Location delivered.')  
+            // re-enable location button
+            $shareLocationButton.removeAttribute('disabled')
         })
     })
 })
